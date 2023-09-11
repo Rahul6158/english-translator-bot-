f0dca81654a7ccf5bc62bc8ef5a13cd8ca9d16a7
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-11 14:16:51 +0530

    Update translator.py

--- translator.py
+++ translator.py
@@ -92,7 +92,8 @@ def get_binary_file_downloader_html(link_text, file_path, file_format):
     return download_link
 
 def main():
-    st.image("jangirii.png", width=50) st.write("Text Translation and Conversion to speech")
+    st.image("jangirii.png", width=50)
+    st.write("Text Translation and Conversion to speech")
     
     # Get user input
     text = st.text_area("Enter text to translate and convert to speech:")
